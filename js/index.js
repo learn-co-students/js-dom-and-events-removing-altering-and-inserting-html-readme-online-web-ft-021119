2ad3e69e2ade323030837e20495c3d14862342c9
bebopvinh <vinhdo07@gmail.com>
2019-05-27 16:58:28 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+function createDivClass1() {
+   return document.createElement('div.class1')
+}
